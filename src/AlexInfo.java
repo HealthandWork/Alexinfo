518dcd6eb8a7bb0c68eb6c8ea0e018e4f1ac5d64
Alexander Rinnström <alexander.rinnstrom.5165@student.uu.se>
2021-11-02 13:26:48 +0100

    Initial commit

---
+++ src/AlexInfo.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class AlexInfo {
+    public static void main (String[] arg) {
+        JOptionPane.showMessageDialog(null, "Alexander Rinnström" + "\nBrunnsgatan 42D" + "\n0734386200");
+    }
+}
